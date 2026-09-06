bb90abb3d4cf0b0f087ffd3b50a88d60be241a7b
LJW <284453795@qq.com>
2026-09-06 16:01:43 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ done
 
 curl -s -L "$BASE_URL/geoip/classical/cn.list" -o "geoip/cn.list"
 
-DOMAIN_RULE_SET=("cn" "microsoft@cn" "apple" "google@cn" "private" "tld-cn" "category-games@cn" "category-ai-!cn" "github" "youtube" "google" "onedrive" "microsoft" "tiktok" "ookla-speedtest" "telegram" "netflix" "paypal" "gfw" "category-porn")
+DOMAIN_RULE_SET=("cn" "geolocation-cn" "geolocation-!cn" "microsoft@cn" "apple" "google@cn" "private" "tld-cn" "category-games@cn" "category-ai-!cn" "github" "youtube" "google" "onedrive" "microsoft" "tiktok" "ookla-speedtest" "telegram" "netflix" "paypal" "gfw" "category-porn")
 
 for DOMAIN_SET in "${DOMAIN_RULE_SET[@]}"; do
   curl -s -L "$BASE_URL/geosite/classical/$DOMAIN_SET.list" -o "geosite/$DOMAIN_SET.list"
