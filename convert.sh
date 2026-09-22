2f75d81c045e598768cb458e271fae132b09ea02
ljw <ljw@ljw.com>
2026-09-22 15:50:44 +0800

    add

---
+++ convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./mihomo-mac convert-ruleset domain text meta/geosite/direct.list meta/geosite/direct.mrs
+./mihomo-mac convert-ruleset domain text meta/geosite/proxy.list meta/geosite/proxy.mrs
+./mihomo-mac convert-ruleset domain text meta/geosite/fakeip-filter-lite.list meta/geosite/fakeip-filter-lite.mrs
+./mihomo-mac convert-ruleset ipcidr text meta/geoip/proxy.list meta/geoip/proxy.mrs
+./mihomo-mac convert-ruleset ipcidr text meta/geoip/direct.list meta/geoip/direct.mrs
+
+./sing-box-mac rule-set compile sing/geosite/direct.json
+./sing-box-mac rule-set compile sing/geosite/proxy.json
+./sing-box-mac rule-set compile sing/geosite/fakeip-filter-lite.json
+./sing-box-mac rule-set compile sing/geoip/proxy.json
+./sing-box-mac rule-set compile sing/geoip/direct.json
