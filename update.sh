d606c74d68750892407cb79496030566330b895e
ljw <ljw@ljw.com>
2026-09-22 15:55:38 +0800

    add

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IP_RULE_SET=("google" "netflix" "telegram" "cn")
+BASE_URL="https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo"
+
+for IP_SET in "${IP_RULE_SET[@]}"; do
+  curl -s -L "$BASE_URL/geoip/$IP_SET.srs" -o "geoip/$IP_SET.srs"
+  curl -s -L "$BASE_URL/geoip/$IP_SET.json" -o "geoip/$IP_SET.json"
+done
+
+DOMAIN_RULE_SET=("cn" "geolocation-cn" "geolocation-!cn" "microsoft@cn" "apple" "google@cn" "private" "tld-cn" "category-games@cn" "category-ai-!cn" "github" "youtube" "google" "onedrive" "microsoft" "tiktok" "ookla-speedtest" "telegram" "netflix" "paypal" "gfw" "category-porn")
+
+for DOMAIN_SET in "${DOMAIN_RULE_SET[@]}"; do
+  curl -s -L "$BASE_URL/geosite/$DOMAIN_SET.srs" -o "geosite/$DOMAIN_SET.srs"
+  curl -s -L "$BASE_URL/geosite/$DOMAIN_SET.json" -o "geosite/$DOMAIN_SET.json"
+done
